9777d33f712d1b70e1d7d72d2eeddb8817ca93ba
Narikodan <108608570+Narikodan@users.noreply.github.com>
2023-01-31 23:11:33 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-var audio1 = new Audio("beep.mp3");
+var audio1 = new Audio("beep2.wav");
 var audio = new Audio("beep3.mp3");
 var audio2 = new Audio("btn4.mp3");
 var audio3 = new Audio("oprtr.mp3");
